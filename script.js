22c58ffa16d5b7909ce365658bba132e9e8e11e6
mateusaffonso <augustomateus@poli.ufrj.br>
2022-01-18 17:35:13 -0300

    First commit

---
+++ script.js
@@ -0,0 +1,52 @@
+
+//1 - Criar um loop que conte de 1 até 10 usando FOR
+
+for(i = 1; i <= 10; i++ ){
+    console.log(i)
+}
+
+
+//2 - Criar um loop que conte de 10 até 1 usando WHILE
+
+let k = 10
+
+while(k > 0){
+    console.log(k)
+    k--
+}
+
+
+//3 - Criar um loop que conte todos os números ímpares de 1 até 100
+
+//usando o FOR
+
+for(z = 1; (z<=100); z = z+2){
+    console.log(z)
+}
+
+//usando while
+
+contador = 1
+while(contador<=100){
+    console.log(contador)
+    contador = contador + 2
+
+}
+
+
+//4- Criar um loop que conte todos os números pares de 0 a 100
+
+//usando o FOR
+
+for(x = 0; x<=100; x = x+2){
+    console.log(x)
+}
+
+//usando while
+
+let j = 0
+while(j<=100){
+    console.log(j)
+    j = j + 2
+
+}
